d9311b0df7ad86aad9d5928daaaffcbdc5a9b67b
Bilostenko <bilostenkod@gmail.com>
2023-11-14 12:41:49 +0200

    redux in JS

--- src/index.js
+++ src/index.js
@@ -1,17 +1,67 @@
+import { click } from '@testing-library/user-event/dist/click';
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const initialState = {value: 0}
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "INC":
+      return {
+        ...state,
+        value: state.value + 1
+      };
+    case "DEC":
+      return {
+        ...state,
+        value: state.value - 1
+      };
+    case "RNDM":
+      return {
+        ...state,
+        value: state.value * action.payload
+      };
+      
+    default:
+      return state
+
+  }
+}
+
+
+const store = createStore(reducer)
+
+const update = () => {
+  document.getElementById("counter").textContent = store.getState().value
+}
+
+store.subscribe(update)
+
+const inc = ()=> ({ type: "INC" });
+const dec = ()=> ({ type: "DEC" });
+const rndm = (value)=> ({ type: "RNDM", payload:value });
+
+document.getElementById("inc").addEventListener("click", () => {
+    store.dispatch(inc())
+  })
+document.getElementById("dec").addEventListener("click", () => {
+    store.dispatch(dec())
+  })
+
+document.getElementById("rndm").addEventListener("click", () => {
+  const value = Math.floor(Math.random() * 10)
+    store.dispatch(rndm(value))
+  })
+
+
+
+ReactDOM.render(
+    <React.StrictMode>
+      <>
+
+      </>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
